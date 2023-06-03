f6bd2535fae6d6097deda72677db9f971c6e6d7a
Nate Sesti <sestinj@gmail.com>
2023-06-02 23:51:06 -0400

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+class Calculator:
+
+    def add(self, num1, num2):
+        return num1 + num2
+
+    def sub(self, num1, num2):
+        return num1 - num2
+
+    def mul(self, num1, num2):
+        return num1 * num2
+
+    def div(self, num1, num2):
+        return num1 / num2
+
+    def exp(self, num1, num2):
+        return num1 ** num2
